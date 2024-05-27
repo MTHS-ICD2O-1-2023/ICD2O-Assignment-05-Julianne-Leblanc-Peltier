8f399d66d3874d7e786577d8f4ec1e63503b092a
Julianne <144264326+Julianne-Leblanc-Peltier@users.noreply.github.com>
2024-05-27 18:49:51 +0000

    started js

--- js/script.js
+++ js/script.js
@@ -3,3 +3,22 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+function myButtonClicked() {
+  const NUMERATOR = 4
+  let denomenator = 1
+  let counter = 0
+  let answer = null
+  let numberOfTimes = parseInt(document.getElementById("number-of-times").value)
+
+  while (true) {
+    if (numberOfTimes = counter) {
+      break
+    }
+    denomenator += 2
+    answer = NUMERATOR / denomenator
+    answer *= -1
+
+    counter += 1
+  }
+}
