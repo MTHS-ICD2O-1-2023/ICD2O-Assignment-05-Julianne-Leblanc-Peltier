aa677b9d4ae20586083056e518e49e3a4173f460
Julianne <144264326+Julianne-Leblanc-Peltier@users.noreply.github.com>
2024-05-28 04:55:15 +0000

    created storyboard + flowchart

--- js/script.js
+++ js/script.js
@@ -5,15 +5,19 @@
 // This file contains the JS functions for index.html
 
 function myButtonClicked() {
+  // variables
   const NUMERATOR = 4
   let denominator = 1
   let answer = NUMERATOR / denominator
   let operator = 1
-  let output = " 4 / 1"
-  const numberOfTimes = parseInt(document.getElementById("number-of-times").value)
+  let output = " (4 / 1)"
   document.getElementById("output").innerHTML = null
   document.getElementById("answer").innerHTML = null
 
+  // input
+  const numberOfTimes = parseInt(document.getElementById("number-of-times").value)
+
+  // process
   for (counter = 1; counter < numberOfTimes; counter++) {
     operator *= -1
     denominator += 2
@@ -25,6 +29,7 @@ function myButtonClicked() {
     }
   }
 
+  // output
   document.getElementById("output").innerHTML = output
   document.getElementById("answer").innerHTML = " = " + answer
 }
