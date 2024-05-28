24f956688c871e0d77d46ba7e86940f87bca3060
Julianne <144264326+Julianne-Leblanc-Peltier@users.noreply.github.com>
2024-05-28 03:57:19 +0000

    fixed js better

--- js/script.js
+++ js/script.js
@@ -9,13 +9,22 @@ function myButtonClicked() {
   let denominator = 1
   let answer = NUMERATOR / denominator
   let operator = 1
+  let output = " 4 / 1"
   const numberOfTimes = parseInt(document.getElementById("number-of-times").value)
+  document.getElementById("output").innerHTML = null
+  document.getElementById("answer").innerHTML = null
 
   for (counter = 1; counter < numberOfTimes; counter++) {
     operator *= -1
     denominator += 2
     answer += (operator * NUMERATOR) / denominator
+    if (operator < 0) {
+      output += " - " + "(" + NUMERATOR + " / " + denominator + ")"
+    } else {
+      output += " + " + "(" + NUMERATOR + " / " + denominator + ")"
+    }
   }
 
-  document.getElementById("answer").innerHTML = answer
+  document.getElementById("output").innerHTML = output
+  document.getElementById("answer").innerHTML = " = " + answer
 }
