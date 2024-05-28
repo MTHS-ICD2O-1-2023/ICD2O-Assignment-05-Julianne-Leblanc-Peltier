31d81103f67bb4259ac1eb7451af035169c795a5
Julianne <144264326+Julianne-Leblanc-Peltier@users.noreply.github.com>
2024-05-28 02:56:57 +0000

    fixed js

--- js/script.js
+++ js/script.js
@@ -1,24 +1,21 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Julianne Leblanc-Peltier
+// Created on: May 2024
 // This file contains the JS functions for index.html
 
 function myButtonClicked() {
   const NUMERATOR = 4
-  let denomenator = 1
-  let counter = 0
-  let answer = null
-  let numberOfTimes = parseInt(document.getElementById("number-of-times").value)
+  let denominator = 1
+  let answer = NUMERATOR / denominator
+  let operator = 1
+  const numberOfTimes = parseInt(document.getElementById("number-of-times").value)
 
-  while (true) {
-    if (numberOfTimes = counter) {
-      break
-    }
-    denomenator += 2
-    answer = NUMERATOR / denomenator
-    answer *= -1
-
-    counter += 1
+  for (counter = 1; counter < numberOfTimes; counter++) {
+    operator *= -1
+    denominator += 2
+    answer += (operator * NUMERATOR) / denominator
   }
+
+  document.getElementById("answer").innerHTML = answer
 }
